af32533b116e308706163421b2e038b090316da1
Phaks323@gmail.com <phaks323@gmail.com@gmail.com>
2024-11-23 19:43:31 +0200

    edqwdf4ef

--- server.py
+++ server.py
@@ -13,7 +13,7 @@ from dotenv import load_dotenv
 load_dotenv()
 app = Flask(__name__)
 
-CORS(app, resources={r"/*": {"origins": "http://localhost:5175"}})
+CORS(app, resources={r"/*": {"origins": "http://localhost:5173"}})
 
 known_faces = []
 
@@ -94,9 +94,9 @@ def recognize_face():
 
                     if result is None:
                         cur.execute(
-                            '''INSERT INTO admin (name, userid, date, datetime)
-                               VALUES (%s, %s, %s, %s)''',
-                            (known_face['name'], 1, current_date, current_datetime)
+                            '''INSERT INTO admin (name, userid, date, datetime, cohort_id)
+                               VALUES (%s, %s, %s, %s, %s)''',
+                            (known_face['name'], 1, current_date, current_datetime, 11)
                         )
                         conn.commit()
 
@@ -120,7 +120,6 @@ def recognize_face():
             })
 
     return jsonify({"faces": faces_data})
-
 
 @app.route('/cohorts', methods=['POST'])
 def cohorts():
@@ -151,7 +150,6 @@ def cohorts():
             cur.close()
             conn.close()
 
-
 @app.route('/upload-image', methods=['POST'])
 def upload_image():
     if 'image' not in request.files:
@@ -171,12 +169,14 @@ def upload_image():
         image_data = image_file.read()
         conn = get_db_connection()
         cur = conn.cursor()
-        
+        print('dfscds')
+
         cur.execute(
             '''INSERT INTO learners ( name, surname, lid, cohort_id, email, image_data)
                VALUES (%s, %s, %s, %s, %s, %s)''',
-            (name, surname, lid, int(cohort), email, psycopg2.Binary(image_data))
+            (name, surname, int(lid), int(cohort), email, psycopg2.Binary(image_data))
         )
+
         conn.commit()
 
         return jsonify({"name": name}), 200
@@ -235,6 +235,39 @@ def get_data():
 
     return jsonify(data)
 
+@app.route('/search', methods=['GET'])
+def search():
+    query = request.args.get('query', '')
+    if not query:
+        return jsonify({"error": "Search query is required"}), 400
+
+    connection = get_db_connection()
+    cursor = connection.cursor()
+
+    cursor.execute('''
+        SELECT lr.learner_id, lr.name, lr.surname, st.sitename, ch.cohortname, lr.lid, lr.email FROM learners as lr
+                        join cohorts as ch  using(cohort_id) 
+                        join sites as st using(site_id) 
+        WHERE lr.name ILIKE %s OR lr.email ILIKE %s OR lr.surname ILIKE %s
+    ''', (f'%{query}%',f'%{query}%',f'%{query}%'))
+
+    rows = cursor.fetchall()
+    data = [
+        { "id" : row[0],
+            "name": row[1],
+            "surname": row[2],
+            "site": row[3],
+            "cohort": row[4],
+            "lid": row[5],
+            "email": row[6]}
+        for row in rows
+    ]
+
+    cursor.close()
+    connection.close()
+
+    return jsonify(data)
+
 @app.route('/users/<int:id>', methods=['GET'])
 def users(id):
     connection = get_db_connection()
@@ -267,6 +300,53 @@ def users(id):
     connection.close()
 
     return jsonify(data)
+
+@app.route('/data', methods=['GET'])
+def get_data_for_date():
+    print('fvdf')
+    date = request.args.get('date')  # Get date from the query parameters (e.g., YYYY-MM-DD)
+    cohort = request.args.get('cohort')
+    if not date:
+        return jsonify({"error": "Date is required"}), 400
+
+    try:
+        # Validate the date format
+        datetime.strptime(date, "%Y-%m-%d")
+        
+        # Connect to the PostgreSQL database
+        conn = get_db_connection()
+        cursor = conn.cursor()
+
+        # Query the database for records that match the selected date
+        cursor.execute("SELECT * FROM admin WHERE date = %s and cohort_id = %s", (date,cohort,))
+
+        # Fetch the results
+        rows = cursor.fetchall()
+
+        data = [
+        {
+            "id" : row[0],
+            "name": row[1],
+            "surname": row[2],
+            "site": row[3],
+            "cohort": row[4],
+            "lid": row[4],
+            "email": row[3],
+        }
+        for row in rows
+    ]
+
+        # Close the database connection
+        cursor.close()
+        conn.close()
+
+        # Return the results as JSON
+        return jsonify(data)
+
+    except psycopg2.Error as e:
+        return jsonify({"error": str(e)}), 500
+    except ValueError:
+        return jsonify({"error": "Invalid date format"}), 400
 
 @app.route('/delet/<int:id>', methods=['DELETE'])
 def delet(id):
